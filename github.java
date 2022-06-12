d0bdce80fd328ae195d8ff2da78cef83b3f6afe1
rdrocks7 <106860440+rdrocks7@users.noreply.github.com>
2022-06-13 01:05:16 +0530

    Add files via upload

---
+++ github.java
@@ -0,0 +1,10 @@
+package study.Student;
+
+public class github {
+
+	public static void main(String[] args) {
+		System.out.println("Check whether gitHub works with virtualization for java files");
+
+	}
+
+}
